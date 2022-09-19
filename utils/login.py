30e098323e980a3258c9f29ba1682c0a3e0990f3
WSpie <lipai.huang14@gmail.com>
2022-09-19 01:07:14 -0500

    add random

--- utils/login.py
+++ utils/login.py
@@ -1,11 +1,14 @@
 import time, sys
+import random
 login_url = 'https://www.linkedin.com/login'
 
 def login(driver, config):
     driver.get(login_url)
     driver.implicitly_wait(10)
+    time.sleep(random.randint(1, 3))
     driver.find_element_by_xpath('//*[@id="username"]').send_keys(config.username)
     driver.find_element_by_xpath('//*[@id="password"]').send_keys(config.password)
+    time.sleep(random.randint(1, 3))
     driver.find_element_by_xpath('//*[@id="organic-div"]/form/div[3]/button').click()
     driver.implicitly_wait(10)
     # check if succeeed
